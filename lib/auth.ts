758715cad25b0bd882b9b230e964f2f68e69910d
Nahuel Frias <nahuelfrias@outlook.com.ar>
2026-02-17 08:27:41 -0300

    agrego lista de urls para better auth no se rompa

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,15 @@ import clientPromise from "./mongodb";
 
 export const auth = betterAuth({
     database: mongodbAdapter((await clientPromise).db("wortise")),
+    
+    baseURL: process.env.BETTER_AUTH_URL,
+
+    trustedOrigins: [
+        "http://localhost:3000",
+        process.env.BETTER_AUTH_URL || "",
+        process.env.NEXT_PUBLIC_APP_URL || "",
+    ].filter(Boolean), 
+
     emailAndPassword: {  
         enabled: true 
     }
