58e4dd89e0eeb5378143599ea7eb64adbb49b878
Nahuel Frias <nahuelfrias@outlook.com.ar>
2026-02-17 08:03:59 -0300

    tambien cambio el hardcodeo en trcp client

--- components/TrpcProvider.tsx
+++ components/TrpcProvider.tsx
@@ -12,7 +12,7 @@ export default function TrpcProvider({ children }: { children: React.ReactNode }
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: 'http://localhost:3000/api/trpc', 
+          url: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000" + "/api/trpc", 
         }),
       ],
     })
